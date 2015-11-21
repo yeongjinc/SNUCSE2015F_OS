e17f5aac24f9ad807d360daafe68613ee14265e0
yeongjinc <yeongjinc@gmail.com>
2015-11-21 16:35:04 +0900

    All 121 tests passed

--- src/filesys/free-map.c
+++ src/filesys/free-map.c
@@ -28,6 +28,8 @@ bool
 free_map_allocate (size_t cnt, block_sector_t *sectorp)
 {
   block_sector_t sector = bitmap_scan_and_flip (free_map, 0, cnt, false);
+  if(sector > 2000)
+	  sector = BITMAP_ERROR;
   if (sector != BITMAP_ERROR
       && free_map_file != NULL
       && !bitmap_write (free_map, free_map_file))
